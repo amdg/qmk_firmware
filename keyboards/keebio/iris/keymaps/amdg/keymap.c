ee7981aafec04385184a010ee36a8dbd3a0e9e5b
amdg <amdg@users.noreply.github.com>
2020-01-04 21:58:25 +0800

    Optimize tapping behavior and some key locations

--- keyboards/keebio/iris/keymaps/amdg/keymap.c
+++ keyboards/keebio/iris/keymaps/amdg/keymap.c
@@ -8,33 +8,31 @@ enum tap_dance_keycodes {
   TD_PSCR = 0
 };
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_PSCR] = ACTION_TAP_DANCE_DOUBLE(KC_PSCR, KC_CAPS)
-};
-
-#define FN_BSPC LT(_FN, KC_BSPC)
+#define FN_QUOTE LALT_T(KC_QUOTE)
 #define FN_ENT RSFT_T(KC_ENT)
+#define FN_MINUS LT(_FN, KC_MINUS)
 #define FN_SPC LSFT_T(KC_SPC)
 
+// FIXME APP_NEXT and APP_PREV don't work as expected because alt needs to be held down in order to progress through the list
 #define APP_NEXT LALT(KC_TAB)
-#define APP_PREV LSFT(LALT(KC_TAB))
+#define APP_PREV LALT(LSFT(KC_TAB))
 #define DSK_NEXT LGUI(LCTL(KC_RIGHT))
 #define DSK_PREV LGUI(LCTL(KC_LEFT))
-#define TAB_NEXT LCTL(KC_PGUP)
-#define TAB_PREV LCTL(KC_PGDN)
+#define TAB_NEXT LCTL(KC_TAB)
+#define TAB_PREV LCTL(LSFT(KC_TAB))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-       KC_TILDE,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,                           KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_UNDS ,KC_EQUAL,TD_PSCR ,
+       KC_TILDE,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,                           KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_UNDS ,KC_EQUAL,KC_PSCR ,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,                           KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                           KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOTE,
+       KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                           KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_BSPC ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_LALT ,         KC_RALT ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RCTL ,
+       KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,FN_QUOTE,         KC_RALT ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RCTL ,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                      KC_LGUI ,TT(_EXT),FN_SPC  ,                  FN_ENT  ,FN_BSPC ,KC_APP
+                                      KC_LGUI ,TT(_EXT),FN_SPC  ,                  FN_ENT  ,FN_MINUS,KC_PLUS
                                   // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -42,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_GRAVE,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                           KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       _______ ,_______ ,KC_PGUP ,KC_LCBR ,KC_LBRC ,KC_LPRN ,                           KC_RPRN ,KC_RBRC ,KC_RCBR ,KC_MINUS,KC_PLUS ,_______ ,
+       _______ ,_______ ,KC_PGUP ,KC_LCBR ,KC_LBRC ,KC_LPRN ,                           KC_RPRN ,KC_RBRC ,KC_RCBR ,_______ ,_______ ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        _______ ,KC_HOME ,KC_PGDN ,KC_END  ,KC_INS  ,KC_DEL  ,                           KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,_______ ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
